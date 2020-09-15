57765772e5ac6b30da9bbefe1ad624b0a226dd2b
Endo <bmw523iboxing@gmail.com>
2020-09-15 15:19:51 +0900

    編集機能 edit, update を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+  
   def show
     @user = User.find(params[:id])
   end
