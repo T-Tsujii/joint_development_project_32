3c0172d19b59a2fd25bc9ae1cfb635c8e5241529
Maruko-HP <hp_maru@icloud.com>
2020-09-19 23:03:48 +0900

    新規タスクの投稿機能 new, create を実装（ストロングパラメータを利用）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,18 @@
 class TasksController < ApplicationController
+  # タスクの新規登録アクション
+  def new
+    @task = Task.new(task_params)
+  end
+
+  # タスクの登録完了アクション
+  def create
+    Task.create(task_params)
+  end
+
+  # StrongParameterの記述部分
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
